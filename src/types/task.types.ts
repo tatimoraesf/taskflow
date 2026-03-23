7fc1ff0e84ccc9740ce7ac2d600d3fc7e87a7e8e
Tatiane Moraes <tatiane.moraes@checkstore.com.br>
2026-03-23 13:52:38 -0300

    estrutura de pastas e tipos de tarefas criados

---
+++ src/types/task.types.ts
@@ -0,0 +1,13 @@
+export type TaskStatus = 'todo' | 'in-progress' | 'done';
+
+export type TaskPriority = 'low' | 'medium' | 'high';
+
+export interface Task {
+  id: string;
+  title: string;
+  status: TaskStatus;
+  priority: TaskPriority;
+  createdAt: Date;
+  completedAt?: Date;
+  description?: string;
+}
