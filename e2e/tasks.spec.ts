a27dbfeae2882e3bc2e76fdbd8e04241bc7cc007
Tatiane Moraes <tatiane.moraes@checkstore.com.br>
2026-05-08 16:03:15 -0300

    test: adiciona teste E2E de editar tarefa

--- e2e/tasks.spec.ts
+++ e2e/tasks.spec.ts
@@ -9,4 +9,18 @@ test.describe('Tarefas', () => {
     await expect(page.getByText('Tarefa adicionada com sucesso!')).toBeVisible()
     await expect(page.getByText('Nova Tarefa')).toBeVisible();
   });
+  
+  test('deve editar uma tarefa com sucesso', async ({ page }) => {
+    await page.goto('/');
+    await page.getByPlaceholder("Ex: Estudar Typescript").fill('Tarefa para Editar');
+    await page.getByRole('button', { name: /adicionar tarefa/i }).click();
+
+    await page.getByLabel('editar tarefa').click();
+    await page.locator('[aria-label="Editar tarefa"]').click();
+    await page.locator('.modal').getByLabel('Título').fill('Tarefa Editada');
+    await page.getByRole('button', { name: /salvar alterações/i }).click();
+
+    await expect(page.getByText('Tarefa atualizada com sucesso!')).toBeVisible();
+    await expect(page.getByText('Tarefa Editada')).toBeVisible();
+  });
 });
